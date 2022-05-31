290848acfc0cc0ff4fe9ccf733a33419aa1a2d80
M Kuznetcov <michail.kuznetsov@gmail.com>
2022-05-31 21:44:39 +0200

    add async call test

--- src/services/product.js
+++ src/services/product.js
@@ -1,2 +1,3 @@
-export const getProducts = () =>
-  fetch("https://fakestoreapi.com/products").then((res) => res.json());
+import axios from "axios";
+
+export const getProducts = () => axios.get("https://fakestoreapi.com/products");
--- tests/unit/CategoryPage.spec.js
+++ tests/unit/CategoryPage.spec.js
@@ -1,18 +1,28 @@
-import { mount } from "@vue/test-utils";
+import { mount, flushPromises } from "@vue/test-utils";
 // import { nextTick } from "vue";
+import axios from "axios";
 import CategoryPage from "@/views/CategoryPage.vue";
 
 import { setActivePinia, createPinia } from "pinia";
 // import { createTestingPinia } from "@pinia/testing";
 // import { useUserStore } from "@/pinia/user";
 
+jest.spyOn(axios, "get").mockResolvedValue([
+  {
+    id: 42,
+    title: "Product 42",
+  },
+]);
+
 describe("CategoryPage.vue", () => {
   beforeEach(() => {
     setActivePinia(createPinia());
   });
 
-  it("renders CategoryPage", () => {
+  it("renders CategoryPage", async () => {
     const wrapper = mount(CategoryPage);
     expect(wrapper.text()).toContain("Products");
+    await flushPromises();
+    expect(wrapper.text()).toContain("Product 42");
   });
 });
