ad498c80c6bd23f5787df99dfe658d47aaf4ea57
M Kuznetcov <michail.kuznetsov@gmail.com>
2022-06-03 12:48:37 +0200

    add bike products

--- src/producs.js
+++ src/producs.js
@@ -1,26 +1,27 @@
 export const products = [
   {
     id: 1,
-    title: "Dingo Dog Bones",
-    image: "https://s.cdpn.io/3/dingo-dog-bones.jpg",
-    price: 12.99,
+    title: "Diamant 137 - Touringbike - 2022 - oxide green metallic",
+    image:
+      "https://images.bike24.net/media/1440/i/mb/63/cd/d3/137hersgn-oxidgruenmetallic-1096346.jpg",
+    price: 2003,
     quantity: 1,
   },
   {
     id: 2,
-    title: "Nutro™ Adult Lamb and Rice Dog Food",
+    title: "ION Bike Helmet Traze AMP MIPS EU/CE - Black",
     image:
-      "https://s.cdpn.io/3/large-NutroNaturalChoiceAdultLambMealandRiceDryDogFood.png",
-    price: 45.99,
+      "https://images.bike24.net/media/1440/i/mb/29/0f/8a/ion-bike-helmet-traze-amp-mips-eu-ce-black-5-1049141.jpg",
+    price: 138.28,
     quantity: 1,
   },
 ];
 
 export const product3 = {
   id: 3,
-  title: "Chicken, Liver & Shrimp Recipe Paté",
+  title: "Hammerhead Karoo 2 GPS Bike Computer",
   image:
-    "https://nutro.com/-/media/barefootpetcare/nutro/products/cat/natural-wet-cat-food/chicken-liver-and-shrimp-recipe-pate-79105115179/carousel/2-1-21-79105118514-gif.gif?h=500&w=500&hash=4FCD071B5123C7436939B2F6FE319756848E6686",
-  price: 18.99,
+    "https://images.bike24.net/media/1440/i/mb/62/6d/a5/hammerhead-karoo2-bike-computer-6-1235503.jpg",
+  price: 405.71,
   quantity: 0,
 };
